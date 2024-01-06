9587963f3bb2d411c95791ab23c5ef950cc74852
SarahBassing <sarah.bassing@gmail.com>
2024-01-05 19:15:03 -0800

    split up homesites, identify new sites in same year and which sites are close to each other across years and packs

--- Scripts/Data_prep.R
+++ Scripts/Data_prep.R
@@ -98,10 +98,97 @@
   far_away_home <- subset(homesites_wgs84, !(obs %in% home_exp_intersection$obs))
   #'  2023 den of the Manada del Arroyo pack
   #'  This pair was released in the state of Chihuahua, Mexico so excluding from analyses
+  homesites_wgs84_usa <- filter(homesites_wgs84, Pack != "Manada del Arroyo")
   
+  #'  Split out dens and rendezvous sites
+  dens <- filter(homesites_wgs84_usa, Site_Type == "Den") %>%
+    #'  Flag packs with multiple den sites in the same year and if any were noted 
+    #'  as the natal den
+    mutate(NewDen_SameYear = ifelse(Pack_year == lead(Pack_year), "TRUE", "FALSE"),
+           NewDen_SameYear = ifelse(Pack_year == lag(Pack_year), "TRUE", NewDen_SameYear),
+           NewDen_SameYear = ifelse(is.na(NewDen_SameYear), "FALSE", NewDen_SameYear)#,
+           #Comments = ifelse(grepl("natal", Comments), "Natal den", Comments),
+           ) %>%
+    relocate(NewDen_SameYear, .after = Comments)
+  rnds <- filter(homesites_wgs84_usa, Site_Type == "Rendezvous")
   
+  #'  Save pack-years with >1 den site
+  multiple_dens <- filter(dens, NewDen_SameYear == "TRUE")
+  den_moves <- subset(homesites, (Pack_year %in% multiple_dens$Pack_year)) %>%
+    filter(Site_Type == "Den")
+  write_csv(den_moves, "./Data/Multiple_den_sites.csv")
   
+  #'  Create buffer around each homesite (width in meters)
+  st_bbox(homesites_wgs84_usa)
+  homesite_buffers <- homesites_wgs84_usa %>%
+    vect() %>%
+    terra::buffer(width = 1000) %>%
+    st_as_sf()
+  ggplot(exp_pop) + geom_sf() + 
+    geom_sf(data = dens, aes(color = Year), shape = 16) +
+    geom_sf(data = homesite_buffers[homesite_buffers$Site_Type == "Den",], colour = "black", fill = NA) +
+    coord_sf(xlim = c(-109.8417, -107.3039), ylim = c(33.0215, 34.2875), expand = FALSE)
+  ggplot(exp_pop) + geom_sf() + 
+    geom_sf(data = rnds, aes(color = Year), shape = 16) +
+    geom_sf(data = homesite_buffers[homesite_buffers$Site_Type == "Rendezvous",], colour = "black", fill = NA) +
+    coord_sf(xlim = c(-109.8417, -107.3039), ylim = c(33.0215, 34.2875), expand = FALSE)
   
+  #'  Which homesites are within 250m of each other?  
+  close_sites <- function(sites) {
+    #'  Create a sequential observation for each unique site
+    pack_yr <- dplyr::select(sites, "Pack_year") %>% 
+      mutate(obs = seq(1:nrow(.))) %>% st_drop_geometry()
+    print(nrow(pack_yr))
+    #'  Calculate pairwise distances between all sites
+    dist_btwn_sites <- sites$geometry %>% 
+      st_distance()
+    #'  Identify which pairings were within 100m of each other
+    indices <- which(dist_btwn_sites <= units::set_units(250, "m"), arr.ind = TRUE)
+    #'  Simplify distance matrix into a dataframe where 1st column = matrix rows,
+    #'  2nd column = matrix columns, and 3rd column = distance IF < 100m between
+    #'  sites indexed by matrix row and column
+    indices_df <- as.data.frame(indices)
+    indices_df$dist <- as.numeric(dist_btwn_sites[indices])
+    #'  Append pack_year as two new columns corresponding to the sites indexed 
+    #'  by the matrix's "row" and "column" 
+    pairwise_distances <- full_join(indices_df, pack_yr, by = c("row" = "obs")) %>%
+      full_join(pack_yr, by = c("col" = "obs")) %>%
+      relocate(Pack_year.x, .before = dist) %>%
+      relocate(Pack_year.y, .before = dist) %>%
+      #'  Pull out just the pack name per site
+      mutate(pack_A = sub("\\_.*", "", Pack_year.x),
+             pack_B = sub("\\_.*", "", Pack_year.y),
+             pack_pair = paste0(Pack_year.x, "_", Pack_year.y)) %>%
+      filter(row != col)
+    
+    return(pairwise_distances)
+  }
+  close_dens <- close_sites(dens)
+  close_rnd <- close_sites(rnds)
   
+  #'  Reduce to packs that used approximately the same site over multiple years 
+  #'  (i.e., recurring use of a site across) or relocated to a site <250m away 
+  #'  in the same year
+  close_dens_skinny <- filter(close_dens, pack_A == pack_B) %>%
+    dplyr::select(c("Pack_year.x", "Pack_year.y", "pack_pair", "dist")) %>%
+    mutate(Repeat_DenYear = duplicated(Pack_year.x))
+  # Need to be able to group dens by pack_years that are spatially overlapping but
+  # in some cases a pack will use the same general area repeatedly for several
+  # years (e.g, 2012-2014 cluster) then move to a new area and repeatedly use
+  # same general area for several more years (e.g., 2015-2016 new cluster). Need
+  # to differentiate these two clusters and then randomly draw one location per
+  # cluster for that pack.
   
+  #'  Filter spatial data to the above sites, then
+  #'  https://stackoverflow.com/questions/77179007/group-spatial-points-by-distance-in-r-how-to-group-cluster-spatial-points-so-gr
+  adj <- st_distance(points)
+  adj <- matrix(as.numeric(as.numeric(adj)) < 11000, nrow = nrow(adj))
+  library(igraph)
+  g <- graph_from_adjacency_matrix(adj)
+  plot(g)
+  df$group <- factor(components(g)$membership)
+  ggplot(rnaturalearth::ne_countries(scale = 10, returnclass = 'sf')) +
+    geom_sf() +
+    geom_point(data = df, aes(x = longitude, y = latitude, color = group)) +
+    coord_sf(xlim = c(71.5, 72.5), ylim = c(-6, -5))
   
