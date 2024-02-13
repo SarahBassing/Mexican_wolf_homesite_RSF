84f8111beb70827e16d5614dc80b5da3ec12c310
Sarah Bassing <sarah.bassing@gmail.com>
2024-02-13 11:27:37 -0800

    update comments

--- Scripts/Covariate_manipulation.R
+++ Scripts/Covariate_manipulation.R
@@ -1,7 +1,10 @@
-  #'  ----------------
+  #'  -----------------------------
   #'  Generate spatial covariates 
   #'  January 2024
-  #'  ----------------
+  #'  -----------------------------
+  #'  Load DEM, seasonal NDVI, and annual forest cover. Generate new terrain variables
+  #'  and format NDVI/forest data for easier extraction.
+  #'  -----------------------------
   
   #'  Load libraries
   library(sf)
@@ -9,10 +12,14 @@
   library(spatialEco)
   library(tidyverse)
   
+  #'  --------------------
+  ####  Load raster data  ####
+  #'  --------------------
   #'  Load spatial data
   dem <- terra::rast("./Shapefiles/Terrain_variables/Mosaic_DEM.tif"); res(dem); st_crs(dem)
   
-  #'  Generate terrain data from DEM
+  #####  Generate terrain data from DEM  #####
+  #'  -----------------------------------
   slope <- terrain(dem, v = "slope", neighbors = 8, unit = "degrees")
   aspect <- terrain(dem, v = "aspect", neighbors = 8, unit = "degrees")
   
@@ -22,11 +29,8 @@
   # writeRaster(slope, "./Shapefiles/Terrain_variables/slope.tif")
   # writeRaster(aspect, "./Shapefiles/Terrain_variables/aspect.tif")
   
-  
-  #'  -------------------------------
-  ####  Calculate terrain variables  ####
-  #'  -------------------------------
   #####  Vector Ruggedness Measure (VRM)  #####
+  #'  -------------------------------------
   #'  Using VRM to represent surface roughness
   #'  1) Decompose DEM into x, y, & z components using trigonometric operators 
   z <- 1 * cos(slope)
@@ -65,67 +69,72 @@
   app(vrm, sd)
   plot(vrm)
   
-  #####  Surface curvatures  #####
-  #'  Gaussian curvature and Profile curvature
-  #'  Code adapted from spatialEco curvature function (https://rdrr.io/cran/spatialEco/src/R/curvature.R)
-  #'  and curvature equations defined in Minar et al. 2020
-  my_curves <- function(x, type = c("planform", "profile", "total", "gaussian", "mcnab", "bolstad"), ...) { 
-    if(!inherits(x, "SpatRaster"))
-      stop(deparse(substitute(x)), " must be a terra SpatRaster object")
-    m <- matrix(1, nrow=3, ncol=3)
-    type = type[1] 
-    if(!any(c("planform", "profile", "total", "gaussian", "mcnab", "bolstad") %in% type)  )
-      stop("Not a valid curvature option")	
-    zt.crv <- function(m, method = type, res = terra::res(x)[1]) {
-      p=(m[6]-m[4])/(2*res)
-      q=(m[2]-m[8])/(2*res)
-      r=(m[4]+m[6]-2*m[5])/(2*(res^2))
-      s=(m[3]+m[7]-m[1]-m[9])/(4*(res^2))
-      tx=(m[2]+m[8]-2*m[5])/(2*(res^2))
-      if(type == "planform") {
-        crv <- round( -(q^2*r-2*p*q*s+p^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)),6) 
-      } else if(type == "profile") {
-        crv <- round( -(p^2*r+2*p*q*s+q^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)^3),6 )
-      } else if(type == "total") {
-        crv <- round( -(q^2*r-2*p*q*s+p^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)),6) + 
-          round( -(p^2*r+2*p*q*s+q^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)^3),6 )  
-      } else if(type == "gaussian"){
-        crv <- round( -(r*tx-s^2)/((1+p^2+q^2)^2),6 )
-      }
-      return(crv)
-    }  	
-    if(type == "bolstad") {
-      return( 10000 * ((x - terra::focal(x, w=m, fun=mean, ...)) / 1000 /
-                         (terra::res[1] + terra::res[1]/2) ) )
-    } else if(type == "mcnab") {
-        mcnab <- function(x) {
-        m <- ceiling(length(x)/2)
-        sum(x[m] - x[-m], na.rm=TRUE) / length(x)-1
-      }
-      tidx <- mask(terra::focal(x, w=m, fun=mcnab, ...), x)
-      return( tidx / as.numeric(terra::global(tidx, "max", na.rm=TRUE)) )
-    }
-    else {
-      return( terra::focal(x, w=m, fun = zt.crv, fillvalue = 0, ...) )
-    }
-  }	
-  gaus_curv <- my_curves(dem, type = "gaussian")
-  plot(gaus_curv)
-  writeRaster(gaus_curv, "./Shapefiles/Terrain_variables/Gaussian_curvature.tif", overwrite = TRUE)
+  #' #####  Surface curvatures  #####
+  #' #'  ------------------------
+  #' #'  Gaussian curvature and Profile curvature
+  #' #'  Code adapted from spatialEco curvature function (https://rdrr.io/cran/spatialEco/src/R/curvature.R)
+  #' #'  and curvature equations defined in Minar et al. 2020
+  #' my_curves <- function(x, type = c("planform", "profile", "total", "gaussian", "mcnab", "bolstad"), ...) { 
+  #'   if(!inherits(x, "SpatRaster"))
+  #'     stop(deparse(substitute(x)), " must be a terra SpatRaster object")
+  #'   m <- matrix(1, nrow=3, ncol=3)
+  #'   type = type[1] 
+  #'   if(!any(c("planform", "profile", "total", "gaussian", "mcnab", "bolstad") %in% type)  )
+  #'     stop("Not a valid curvature option")	
+  #'   zt.crv <- function(m, method = type, res = terra::res(x)[1]) {
+  #'     p=(m[6]-m[4])/(2*res)
+  #'     q=(m[2]-m[8])/(2*res)
+  #'     r=(m[4]+m[6]-2*m[5])/(2*(res^2))
+  #'     s=(m[3]+m[7]-m[1]-m[9])/(4*(res^2))
+  #'     tx=(m[2]+m[8]-2*m[5])/(2*(res^2))
+  #'     if(type == "planform") {
+  #'       crv <- round( -(q^2*r-2*p*q*s+p^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)),6) 
+  #'     } else if(type == "profile") {
+  #'       crv <- round( -(p^2*r+2*p*q*s+q^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)^3),6 )
+  #'     } else if(type == "total") {
+  #'       crv <- round( -(q^2*r-2*p*q*s+p^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)),6) + 
+  #'         round( -(p^2*r+2*p*q*s+q^2*tx)/((p^2+q^2)*sqrt(1+p^2+q^2)^3),6 )  
+  #'     } else if(type == "gaussian"){
+  #'       crv <- round( -(r*tx-s^2)/((1+p^2+q^2)^2),6 )
+  #'     }
+  #'     return(crv)
+  #'   }  	
+  #'   if(type == "bolstad") {
+  #'     return( 10000 * ((x - terra::focal(x, w=m, fun=mean, ...)) / 1000 /
+  #'                        (terra::res[1] + terra::res[1]/2) ) )
+  #'   } else if(type == "mcnab") {
+  #'       mcnab <- function(x) {
+  #'       m <- ceiling(length(x)/2)
+  #'       sum(x[m] - x[-m], na.rm=TRUE) / length(x)-1
+  #'     }
+  #'     tidx <- mask(terra::focal(x, w=m, fun=mcnab, ...), x)
+  #'     return( tidx / as.numeric(terra::global(tidx, "max", na.rm=TRUE)) )
+  #'   }
+  #'   else {
+  #'     return( terra::focal(x, w=m, fun = zt.crv, fillvalue = 0, ...) )
+  #'   }
+  #' }	
+  #' gaus_curv <- my_curves(dem, type = "gaussian")
+  #' plot(gaus_curv)
+  #' writeRaster(gaus_curv, "./Shapefiles/Terrain_variables/Gaussian_curvature.tif", overwrite = TRUE)
+  #' 
+  #' # Converting Minar parameters to spatialEco parameters 
+  #' # p = x
+  #' # q = y
+  #' # r = xx
+  #' # s = xy
+  #' # tx = yy
+  #' 
+  #' #'  Profile curvature: second derivative of elevation surface (slope of the slope)
+  #' #'  i.e., direction of the maximum slope where (-) values indicate surface is 
+  #' #'  upwardly convex and (+) values indicate surface is upwardly concave. Values
+  #' #'  of 0 indicate flat surface.
+  #' profcurv <- curvature(dem, type = "profile")
+  #' writeRaster(profcurv, "./Shapefiles/Terrain_variables/Profile_curvature.tif", overwrite = TRUE)
   
-  # Converting Minar parameters to spatialEco parameters 
-  # p = x
-  # q = y
-  # r = xx
-  # s = xy
-  # tx = yy
   
   
-  #'  Profile curvature: second derivative of elevation surface (slope of the slope)
-  #'  i.e., direction of the maximum slope where (-) values indicate surface is 
-  #'  upwardly convex and (+) values indicate surface is upwardly concave. Values
-  #'  of 0 indicate flat surface.
-  profcurv <- curvature(dem, type = "profile")
-  writeRaster(profcurv, "./Shapefiles/Terrain_variables/Profile_curvature.tif", overwrite = TRUE)
+  
+  
   
   
