c0b896a39bb5598fa7ed45ebd800e3a5455cddee
SarahBassing <sarah.bassing@gmail.com>
2024-02-26 17:42:50 -0800

    Script to conduct sensitivity analysis on number of available locations needed before slope coefficients aren't influenced by sample size

---
+++ Scripts/Sensitivity_analysis_UseAvailable_ratio.R
@@ -0,0 +1,203 @@
+  #'  ----------------------
+  #'  Sensitivity analysis for used:available ratio
+  #'  Mexican wolf project
+  #'  Sarah B. Bassing
+  #'  December 2023
+  #'  ----------------------
+  #'  Randomly sample available points into different use:available ratios and run
+  #'  model to identify the ratio at which slope coefficients asymptope
+  #'  ----------------------
+  
+  #'  Clean work station
+  rm(list =ls())
+  
+  #'  Load packages
+  library(lme4)
+  library(purrr)
+  library(ggplot2)
+  library(tidyverse)
+  
+  #'  Load data
+  data_den <- st_read("./Data/all_data_den.csv")
+  data_rnd <- st_read("./Data/all_data_rnd.csv")
+  
+  #'  Sub-sample data sets to fewer available locations (2, 10, 20, 50, 100, 200, 500) 
+  #'  to evaluate the point at which estimated slope coefficients stabilize
+  subsample_avial <- function(dat, navail) {
+    #'  Pull out available locations
+    avail_locs <- filter(dat, used == 0)
+    
+    #'  Sub-sample number of available locations based on the number of used locations
+    #'  per individual/season/year and a desired ratio of used:available locations
+    sub_sample <- avail_locs %>%
+      group_by(Pack_year) %>%
+      slice_sample(n = navail) %>%
+      ungroup()
+    
+    #'  Append sub-sampled available points to used points and re-organize
+    full_dat <- dat %>%
+      filter(used == 1) %>%
+      bind_rows(sub_sample) %>%
+      arrange(Pack_year)
+    
+    #'  Center and scale covariates
+    full_dat_z <- full_dat %>%
+      transmute(Pack_year = Pack_year,
+                Homesite = Homesite,
+                used = as.numeric(used),
+                wgts = as.numeric(wgts),
+                Elev = scale(as.numeric(Elevation_m)),
+                Slope = scale(as.numeric(Slope_degrees)),
+                Rough = scale(as.numeric(Roughness_VRM)),
+                Curve = scale(as.numeric(Gaussian_curvature)),
+                NearWater = scale(as.numeric(Nearest_water_m)),
+                logNearWater = scale(log(as.numeric(Nearest_water_m))),
+                HumanMod = scale(as.numeric(Human_mod_index)),
+                NearRoad = scale(as.numeric(Nearest_road_m)),
+                logNearRoad = scale(log(as.numeric(Nearest_road_m))))
+    
+    #'  Assess correlation among all scaled variables
+    covs <- full_dat_z %>%
+      dplyr::select(-c(Pack_year, Homesite, used, wgts))
+    cor_matrix <- cor(covs, use = "complete.obs")
+    print(cor_matrix)
+    
+    return(full_dat_z)
+  }
+  #'  Sub-sample available locations 100 times with different numbers of samples
+  den_1_2_ratio <- rerun(50, subsample_avial(data_den, navail = 2))
+  den_1_10_ratio <- rerun(50, subsample_avial(data_den, navail = 10))
+  den_1_20_ratio <- rerun(50, subsample_avial(data_den, navail = 20))
+  den_1_50_ratio <- rerun(50, subsample_avial(data_den, navail = 50))
+  den_1_100_ratio <- rerun(50, subsample_avial(data_den, navail = 100))
+  den_1_200_ratio <- rerun(50, subsample_avial(data_den, navail = 200))
+  den_1_500_ratio <- rerun(50, subsample_avial(data_den, navail = 500))
+  str(den_1_2_ratio[[1]]); head(den_1_2_ratio[[1]])
+  
+  rnd_1_2_ratio <- rerun(50, subsample_avial(data_rnd, navail = 2))
+  rnd_1_10_ratio <- rerun(50, subsample_avial(data_rnd, navail = 10))
+  rnd_1_20_ratio <- rerun(50, subsample_avial(data_rnd, navail = 20))
+  rnd_1_50_ratio <- rerun(50, subsample_avial(data_rnd, navail = 50))
+  rnd_1_100_ratio <- rerun(50, subsample_avial(data_rnd, navail = 100))
+  rnd_1_200_ratio <- rerun(50, subsample_avial(data_rnd, navail = 200))
+  rnd_1_500_ratio <- rerun(50, subsample_avial(data_rnd, navail = 500))
+  head(rnd_1_2_ratio[[1]])
+  
+  #'  Fit basic den model
+  fit_den_rsf <- function(dat){
+    mod <- lm(used ~ Elev + Slope + logNearWater, weight = wgts, data = dat, family = bimomial(link = "logit"))
+    return(mod)
+  }
+  den_1_2_mod <- lapply(den_1_2_ratio, fit_den_rsf); summary(den_1_2_mod[[1]]); car::vif(den_1_2_mod[[1]])
+  den_1_10_mod <- lapply(den_1_10_ratio, fit_den_rsf); summary(den_1_10_mod[[1]]); car::vif(den_1_10_mod[[1]])
+  den_1_20_mod <- lapply(den_1_20_ratio, fit_den_rsf); summary(den_1_20_mod[[1]]); car::vif(den_1_20_mod[[1]])
+  den_1_50_mod <- lapply(den_1_50_ratio, fit_den_rsf); summary(den_1_50_mod[[1]]); car::vif(den_1_50_mod[[1]])
+  den_1_100_mod <- lapply(den_1_100_ratio, fit_den_rsf); summary(den_1_100_mod[[1]]); car::vif(den_1_100_mod[[1]])
+  den_1_200_mod <- lapply(den_1_200_ratio, fit_den_rsf); summary(den_1_200_mod[[1]]); car::vif(den_1_200_mod[[1]])
+  den_1_500_mod <- lapply(den_1_500_ratio, fit_den_rsf); summary(den_1_500_mod[[1]]); car::vif(den_1_500_mod[[1]])
+  
+  #'  Fit Ausband et al. (2010) RSF  ############## ADD NDVI eventually
+  fit_rnd_rsf <- function(dat){
+    mod <- lm(used ~ Rough + Curve, weight = wgts, data = dat, family = bimomial(link = "logit"))
+    return(mod)
+  }
+  rnd_1_2_mod <- lapply(rnd_1_2_ratio, fit_rnd_rsf); summary(rnd_1_2_mod[[1]]); car::vif(rnd_1_2_mod[[1]])
+  rnd_1_10_mod <- lapply(rnd_1_10_ratio, fit_rnd_rsf); summary(rnd_1_10_mod[[1]]); car::vif(rnd_1_10_mod[[1]])
+  rnd_1_20_mod <- lapply(rnd_1_20_ratio, fit_rnd_rsf); summary(rnd_1_20_mod[[1]]); car::vif(rnd_1_20_mod[[1]])
+  rnd_1_50_mod <- lapply(rnd_1_50_ratio, fit_rnd_rsf); summary(rnd_1_50_mod[[1]]); car::vif(rnd_1_50_mod[[1]])
+  rnd_1_100_mod <- lapply(rnd_1_100_ratio, fit_rnd_rsf); summary(rnd_1_100_mod[[1]]); car::vif(rnd_1_100_mod[[1]])
+  rnd_1_200_mod <- lapply(rnd_1_200_ratio, fit_rnd_rsf); summary(rnd_1_200_mod[[1]]); car::vif(rnd_1_200_mod[[1]])
+  rnd_1_500_mod <- lapply(rnd_1_500_ratio, fit_rnd_rsf); summary(rnd_1_500_mod[[1]]); car::vif(rnd_1_500_mod[[1]])
+  
+  #'  Save parameters and combine into single data frame per homesite and ratio
+  # rounddig <- 2
+  rsf_out <- function(mod, navail) {
+    est <- as.data.frame(coef(summary(mod)))
+    mod_out <- est %>%
+      transmute(Parameter = row.names(.),
+                Estimate = Estimate) %>%
+      mutate(Parameter = ifelse(Parameter == "(Intercept)", "Intercept", Parameter),
+             nAvailable = navail)
+    rownames(mod_out) <- NULL
+    return(mod_out)
+  }
+  den_1_2_mod_out <- lapply(den_1_2_mod, rsf_out, navail = 2); den_1_2_mod_out <- do.call(rbind.data.frame, den_1_2_mod_out)
+  den_1_10_mod_out <- lapply(den_1_10_mod, rsf_out, navail = 10); den_1_10_mod_out <- do.call(rbind.data.frame, den_1_10_mod_out)
+  den_1_20_mod_out <- lapply(den_1_20_mod, rsf_out, navail = 20); den_1_20_mod_out <- do.call(rbind.data.frame, den_1_20_mod_out)
+  den_1_50_mod_out <- lapply(den_1_50_mod, rsf_out, navail = 50); den_1_50_mod_out <- do.call(rbind.data.frame, den_1_50_mod_out)
+  den_1_100_mod_out <- lapply(den_1_100_mod, rsf_out, navail = 100); den_1_100_mod_out <- do.call(rbind.data.frame, den_1_100_mod_out)
+  den_1_200_mod_out <- lapply(den_1_200_mod, rsf_out, navail = 200); den_1_200_mod_out <- do.call(rbind.data.frame, den_1_200_mod_out)
+  den_1_500_mod_out <- lapply(den_1_500_mod, rsf_out, navail = 500); den_1_500_mod_out <- do.call(rbind.data.frame, den_1_500_mod_out)
+  
+  rnd_1_2_mod_out <- lapply(rnd_1_2_mod, rsf_out, navail = 2); rnd_1_2_mod_out <- do.call(rbind.data.frame, rnd_1_2_mod_out)
+  rnd_1_10_mod_out <- lapply(rnd_1_10_mod, rsf_out, navail = 10); rnd_1_10_mod_out <- do.call(rbind.data.frame, rnd_1_10_mod_out)
+  rnd_1_20_mod_out <- lapply(rnd_1_20_mod, rsf_out, navail = 20); rnd_1_20_mod_out <- do.call(rbind.data.frame, rnd_1_20_mod_out)
+  rnd_1_50_mod_out <- lapply(rnd_1_50_mod, rsf_out, navail = 50); rnd_1_50_mod_out <- do.call(rbind.data.frame, rnd_1_50_mod_out)
+  rnd_1_100_mod_out <- lapply(rnd_1_100_mod, rsf_out, navail = 100); rnd_1_100_mod_out <- do.call(rbind.data.frame, rnd_1_100_mod_out)
+  rnd_1_200_mod_out <- lapply(rnd_1_200_mod, rsf_out, navail = 200); rnd_1_200_mod_out <- do.call(rbind.data.frame, rnd_1_200_mod_out)
+  rnd_1_500_mod_out <- lapply(rnd_1_500_mod, rsf_out, navail = 500); rnd_1_500_mod_out <- do.call(rbind.data.frame, rnd_1_500_mod_out)
+  
+  #'  Format data for plotting
+  all_coefs <- function(ratio1, ratio2, ratio3, ratio4, ratio5, ratio6, ratio7) {
+    coefs <- bind_rows(ratio1, ratio2, ratio3, ratio4, ratio5, ratio6, ratio7) %>%
+      mutate(nAvailable = factor(nAvailable, levels = c("2", "10", "20", "50", "100", "200", "500")))
+    return(coefs)
+  }
+  den_coefs <- all_coefs(den_1_2_mod_out, den_1_10_mod_out, den_1_20_mod_out, den_1_50_mod_out, 
+                         den_1_100_mod_out, den_1_200_mod_out, den_1_500_mod_out)
+  rnd_coefs <- all_coefs(rnd_1_2_mod_out, rnd_1_10_mod_out, rnd_1_20_mod_out, rnd_1_50_mod_out, 
+                         rnd_1_100_mod_out, rnd_1_200_mod_out, rnd_1_500_mod_out)
+  
+  #'  Plot estimated coefficients as number of available locations increases
+  boxplot_den_dat <- function(df) {
+    #'  Intercept
+    plot_intercept <- ggplot(df[df$Parameter == "Intercept",], aes(factor(nAvailable), y = Estimate)) + 
+      geom_boxplot() + ggtitle("Intercept") + theme_light() +
+      labs(x = "Number of available locations", y = "Average coefficient estimate") 
+    plot(plot_intercept)
+    #'  Slope parameter #1
+    plot_elevation <- ggplot(df[df$Parameter == "Elev",], aes(factor(nAvailable), y = Estimate)) +
+      geom_boxplot() + ggtitle("Elevation") + theme_light() +
+      labs(x = "Number of available locations", y = "Average coefficient estimate") 
+    plot(plot_elevation)
+    #'  Slope parameter #2
+    plot_slope <- ggplot(df[df$Parameter == "Slope",], aes(factor(nAvailable), y = Estimate)) +
+      geom_boxplot() + ggtitle("Slope") + theme_light() +
+      labs(x = "Number of available locations", y = "Average coefficient estimate")
+    plot(plot_slope)
+    #'  Slope parameter #3
+    plot_water <- ggplot(df[df$Parameter == "logNearWater",], aes(factor(nAvailable), y = Estimate)) +
+      geom_boxplot() + ggtitle("Log of the Distance to nearest waterbody") + theme_light() + 
+      ggtitle("Intercept") + labs(x = "Number of available locations", y = "Average coefficient estimate") 
+    plot(plot_water)
+  }
+  den_coef_boxplots <- boxplot_den_dat(den_coefs)
+  
+  boxplot_rnd_dat <- function(df) {
+    #'  Intercept
+    plot_intercept <- ggplot(df[df$Parameter == "Intercept",], aes(factor(nAvailable), y = Estimate)) +
+      geom_boxplot() + ggtitle("Intercept") + theme_light() +
+      labs(x = "Number of available locations", y = "Average coefficient estimate")
+    plot(plot_intercept)
+    #'  Slope parameter #1
+    plot_rough <- ggplot(df[df$Parameter == "Rough",], aes(factor(nAvailable), y = Estimate)) +
+      geom_boxplot() + ggtitle("Roughness (VRM)") + theme_light() +
+      labs(x = "Number of available locations", y = "Average coefficient estimate") 
+    plot(plot_rough)
+    #'  Slope parameter #2
+    plot_curve <- ggplot(df[df$Parameter == "Curve",], aes(factor(nAvailable), y = Estimate)) +
+      geom_boxplot() + ggtitle("Gaussian curvature") + theme_light() +
+      labs(x = "Number of available locations", y = "Average coefficient estimate") 
+    plot(plot_curve)
+    #' #'  Slope parameter #3
+    #' plot_ndvi <- ggplot(df[df$Parameter == "NDVI",], aes(factor(nAvailable), y = Estimate)) +
+    #'   geom_boxplot() + ggtitle("Mean seasonal NDVI") + theme_light() +
+    #'   labs(x = "Number of available locations", y = "Average coefficient estimate") 
+    #' plot(plot_ndvi)
+  }
+  rnd_coef_boxplots <- boxplot_rnd_dat(rnd_coefs)
+  
+  
+  
+  
+  
