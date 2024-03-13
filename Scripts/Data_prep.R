d58b76f9fc7e8dd4ed31bb6191a46343f2af89c8
SarahBassing <sarah.bassing@gmail.com>
2024-03-13 13:03:03 -0700

    move reference raster code to new script, load grid points and ndvi grid data

--- Scripts/Data_prep.R
+++ Scripts/Data_prep.R
@@ -39,15 +39,6 @@
   human_mod <- terra::rast("./Shapefiles/Human_variables/mosaic_global_Human_Modification.tif"); res(human_mod); crs(human_mod)
   roads <- terra::rast("./Shapefiles/Human_variables/mosaic_dist2road.tif"); res(roads); crs(roads)
   
-  #' #'  Create empty raster based on extent, resolution, & coord system of elevation raster
-  #' empty_rast <- rast(elev, nlyrs = nlyr(elev), names = "CellID", vals = 1:ncell(elev), keeptime = FALSE,
-  #'                    keepunits = FALSE, props = FALSE)
-  #' #'  Make sure it checks out
-  #' res(empty_rast); crs(empty_rast); st_bbox(empty_rast); st_bbox(elev)
-  #' #'  Save that sucker
-  #' writeRaster(empty_rast, filename  = "./Shapefiles/Reference_grid_30m.tif", overwrite = TRUE)
-  empty_rast <- terra::rast("./Shapefiles/Reference_grid_30m.tif")
-  
   #'  Define WGS84 coordinate systems
   wgs84 <- st_crs("+proj=longlat +datum=WGS84 +no_defs")
   
@@ -679,79 +670,8 @@
   #'  -----------------------------------------
   ####  Extract covariates for reference grid  ####
   #'  -----------------------------------------
-  #'  Mask out waterbodies
-  masked_water <- mask(empty_rast, bigwater_nad83, inverse = TRUE)
-  plot(masked_water)
-  #'  Mask out unsuitable areas (so areas NOT in the wmep_suitable polygon)
-  wmepa_grid <- mask(masked_water, wmepa_suitable_nad83)
-  plot(wmepa_grid); plot(homesites_nad83[1], add = TRUE)
-  #'  Save
-  writeRaster(wmepa_grid, "./Shapefiles/WMEPA_masked_grid.tif", overwrite = TRUE)
-  
-  #' #'  Extract centroid of each grid cell from WMEPA masked grid
-  #' wmepa_grid <- terra::rast("./Shapefiles/WMEPA_masked_grid.tif")
-  #' #'  Convert to a polygon and sf object
-  #' wmepa_poly <- as.polygons(wmepa_grid)  # THIS TAKES FOREVER
-  #' wmepa_poly_sf <- st_as_sf(wmepa_poly)
-  #' st_write(wmepa_poly_sf, "./Shapefiles/WMEPA_masked_polygon.shp")
-  #' #'  Grab centroid coordinates and create a data frame with cell ID and lat/long
-  #' wmepa_grid_centroid <- st_centroid(wmepa_poly_sf)
-  #' wmepa_grid_coords <- st_coordinates(wmepa_grid_centroid)
-  #' wmepa_grid_cellID <- wmepa_grid_centroid$CellID
-  #' wmepa_grid_cellID <- as.data.frame(wmepa_grid_cellID)
-  #' names(wmepa_grid_cellID) <- "cellID"
-  #' wmepa_grid_pts <- bind_cols(wmepa_grid_cellID, wmepa_grid_coords)
-  #' write_csv(wmepa_grid_pts, file = "./Data/WMEPA_suitable_grid_points.csv")
-  #' #'  Convert data to shapefiles
-  wmepa_grid_pts <- read_csv("./Data/WMEPA_suitable_grid_points.csv")
-  wmepa_grid_pts <- st_as_sf(wmepa_grid_pts, coords = c("X", "Y"), crs = nad83) %>%
-    st_transform(wgs84) %>%
-    mutate(newID = 1:nrow(.))
-  #'  Slit it into more manageable chunks
-  nrow(wmepa_grid_pts)
-  wmepa_grid_pts1aa <- wmepa_grid_pts[1:250000,]
-  wmepa_grid_pts1ab <- wmepa_grid_pts[250001:500000,]
-  wmepa_grid_pts1b <- wmepa_grid_pts[500001:1000000,]
-  wmepa_grid_pts2a <- wmepa_grid_pts[1000001:1500000,]
-  wmepa_grid_pts2ba <- wmepa_grid_pts[1500001:1750000,]
-  wmepa_grid_pts2bb <- wmepa_grid_pts[1750001:2000000,]
-  wmepa_grid_pts3aa <- wmepa_grid_pts[2000001:2250000,]
-  wmepa_grid_pts3ab <- wmepa_grid_pts[2250001:2500000,]
-  wmepa_grid_pts3b <- wmepa_grid_pts[2500001:3000000,]
-  wmepa_grid_pts4a <- wmepa_grid_pts[3000001:3500000,]
-  wmepa_grid_pts4b <- wmepa_grid_pts[3500001:4000000,]
-  wmepa_grid_pts5a <- wmepa_grid_pts[4000001:4500000,]
-  wmepa_grid_pts5b <- wmepa_grid_pts[4500001:5000000,]
-  wmepa_grid_pts6a <- wmepa_grid_pts[5000001:5500000,]
-  wmepa_grid_pts6b <- wmepa_grid_pts[5500001:6000000,]   
-  wmepa_grid_pts7a <- wmepa_grid_pts[6000001:6500000,]
-  wmepa_grid_pts7b <- wmepa_grid_pts[6500001:7000000,]
-  wmepa_grid_pts8a <- wmepa_grid_pts[7000001:7500000,]
-  wmepa_grid_pts8b <- wmepa_grid_pts[7500001:8000000,]
-  wmepa_grid_pts9a <- wmepa_grid_pts[8000001:nrow(wmepa_grid_pts),]
-
-  #'  Save shapefiles
-  st_write(wmepa_grid_pts, "./Shapefiles/WMEPA_grid_points.shp")
-  st_write(wmepa_grid_pts1aa, "./Shapefiles/WMEPA_grid_points1aa.shp")
-  st_write(wmepa_grid_pts1ab, "./Shapefiles/WMEPA_grid_points1ab.shp")
-  st_write(wmepa_grid_pts1b, "./Shapefiles/WMEPA_grid_points1b.shp")
-  st_write(wmepa_grid_pts2a, "./Shapefiles/WMEPA_grid_points2a.shp")
-  st_write(wmepa_grid_pts2ba, "./Shapefiles/WMEPA_grid_points2ba.shp")
-  st_write(wmepa_grid_pts2bb, "./Shapefiles/WMEPA_grid_points2bb.shp")
-  st_write(wmepa_grid_pts3aa, "./Shapefiles/WMEPA_grid_points3aa.shp")
-  st_write(wmepa_grid_pts3ab, "./Shapefiles/WMEPA_grid_points3ab.shp")
-  st_write(wmepa_grid_pts3b, "./Shapefiles/WMEPA_grid_points3b.shp")
-  st_write(wmepa_grid_pts4a, "./Shapefiles/WMEPA_grid_points4a.shp")
-  st_write(wmepa_grid_pts4b, "./Shapefiles/WMEPA_grid_points4b.shp")
-  st_write(wmepa_grid_pts5a, "./Shapefiles/WMEPA_grid_points5a.shp")
-  st_write(wmepa_grid_pts5b, "./Shapefiles/WMEPA_grid_points5b.shp")
-  st_write(wmepa_grid_pts6a, "./Shapefiles/WMEPA_grid_points6a.shp")
-  st_write(wmepa_grid_pts6b, "./Shapefiles/WMEPA_grid_points6b.shp")
-  st_write(wmepa_grid_pts7a, "./Shapefiles/WMEPA_grid_points7a.shp")
-  st_write(wmepa_grid_pts7b, "./Shapefiles/WMEPA_grid_points7b.shp")
-  st_write(wmepa_grid_pts8a, "./Shapefiles/WMEPA_grid_points8a.shp")
-  st_write(wmepa_grid_pts8b, "./Shapefiles/WMEPA_grid_points8b.shp")
-  st_write(wmepa_grid_pts9a, "./Shapefiles/WMEPA_grid_points9a.shp")
+  grid_pts <- st_read("./Shapefiles/MWEPA_suitable_reference_grid.shp"); crs(grid_pts)
+  ndvi_grid <- read_csv("./Data/GEE extracted data/GEE_mean_NDVI_grid_rnd_2023.csv")
   
   
   
