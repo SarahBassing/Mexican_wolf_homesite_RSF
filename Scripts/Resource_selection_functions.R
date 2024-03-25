a470d5839b1202aa0a8d53b74834601a7e7b57fe
SarahBassing <sarah.bassing@gmail.com>
2024-03-25 14:09:32 -0700

    scratch the reduced approach

--- Scripts/Resource_selection_functions.R
+++ Scripts/Resource_selection_functions.R
@@ -218,14 +218,14 @@
     return(out)
   }
   #'  Extract coefficient estimates for each trained model
-  topmod_den_coefs <- top_rsf_out(h4.den_reduced, sitetype = "Den")          # h4.den
-  topmod_rnd_coefs <- top_rsf_out(h2.rnd_reduced, sitetype = "Rendezvous")   # h2.rnd
+  topmod_den_coefs <- top_rsf_out(h4.den, sitetype = "Den")          
+  topmod_rnd_coefs <- top_rsf_out(h2.rnd, sitetype = "Rendezvous")   
   
   #'  Coefficient results table
   topmod_coefs <- bind_rows(topmod_den_coefs, topmod_rnd_coefs)
   
   #'  Write file for publication
-  write_csv(topmod_coefs, file = "./Outputs/Tables/Top_Model_Coefficients_reduced.csv")
+  write_csv(topmod_coefs, file = "./Outputs/Tables/Top_Model_Coefficients.csv")
   
   #####  AIC model ranks  #####
   #'  Generate modelselection objects
@@ -347,8 +347,8 @@
     return(out)
   }
   #'  Extract coefficient estimates for each trained model
-  coefs_h4.den <- rsf_out(h4.den_reduced)   #h4.den
-  coefs_h2.rnd <- rsf_out(h2.rnd_reduced)   #h2.rnd
+  coefs_h4.den <- rsf_out(h4.den)   
+  coefs_h2.rnd <- rsf_out(h2.rnd)   
   
   ####  Predict across suitable habitat ####
   #'  ----------------------------------
